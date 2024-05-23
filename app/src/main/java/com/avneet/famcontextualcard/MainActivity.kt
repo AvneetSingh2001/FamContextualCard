9b7bb47ff1e9b0394a455ea2ed5094c88fd2e1ba
Avneet Singh <avneetmaankiya@gmail.com>
2024-05-23 09:15:33 +0530

    Implemented Scaffold TopBar

--- app/src/main/java/com/avneet/famcontextualcard/MainActivity.kt
+++ app/src/main/java/com/avneet/famcontextualcard/MainActivity.kt
@@ -4,27 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.avneet.famcontextualcard.presentation.ui.screens.cards.CardScreen
 import com.avneet.famcontextualcard.ui.theme.FamContextualCardTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             FamContextualCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        Row(
+                            modifier = Modifier
+                                .background(Color.White)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_fampay_logo),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .height(60.dp)
+                                    .padding(10.dp)
+                            )
+                        }
+                    },
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     CardScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
